7619c403d845dfcf60fc244fd5e355e174994b2b
teddyomondi <theoriginalsomosh@gmail.com>
2024-05-16 08:15:36 +0300

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 class Cache:
     def __init__(self):
@@ -13,11 +13,28 @@ class Cache:
         self._redis.set(key, data)
         return key
 
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        return self.get(key, lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        return self.get(key, int)
+
 # Usage example
 if __name__ == "__main__":
     cache = Cache()
-    data = b"hello"
-    key = cache.store(data)
-    print(key)
-    local_redis = redis.Redis()
-    print(local_redis.get(key))
+    test_cases = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
+    for value, fn in test_cases.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
